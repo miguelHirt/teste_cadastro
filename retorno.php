0190d7b11bc68f34889aae8ab36b67883439197f
miguelHirt <54045986+miguelHirt@users.noreply.github.com>
2021-03-12 10:58:34 -0300

    Update retorno.php

--- retorno.php
+++ retorno.php
@@ -6,16 +6,16 @@
 	$nome = $_POST['nome'];
 	$cpf = $_POST['cpf'];
 	$email = $_POST['email'];
-    $fone = $_POST['fone'];
+    	$fone = $_POST['fone'];
 	$estado = $_POST['estado'];
 	$cep = $_POST['cep'];
-    $estado2 = $_POST['estado2'];
+    	$estado2 = $_POST['estado2'];
 	$municipio = $_POST['municipio'];
 	$bairro = $_POST['bairro'];
-    $rua = $_POST['rua'];
+    	$rua = $_POST['rua'];
 	$numero = $_POST['numero'];
 	$complemento = $_POST['complemento'];
-    $observacao = $_POST['observacao'];
+    	$observacao = $_POST['observacao'];
 	
 	$result = "INSERT INTO cadastro (nome, cpf,email,fone,estado,cep,estado2,municipio,bairro,rua,numero,complemento,observacao) VALUES ('$nome','$cpf','$email','$fone','$estado','$cep','$estado2','$municipio','$bairro','$rua','$numero','$complemento','$observacao')";
 	$resultado = mysqli_query($link, $result);
